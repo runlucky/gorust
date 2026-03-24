06d3c769052fd3bd49bcd7409df266bee02f4943
Kakeru Fukuda <stand.by.we@gmail.com>
2026-03-25 08:53:34 +0900

    13章

---
+++ rust/closure/src/main.rs
@@ -0,0 +1,66 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum ShirtColor {
+    Red,
+    Blue
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>
+}
+
+impl Inventory {
+    /// userが色を指定した場合をそれを、そうでない場合は最も在庫が多い色を返す
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    /// 最も在庫が多い色を返す
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1
+            }
+        }
+
+        if num_blue < num_red {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn 最も在庫が多い色を返すこと() {
+        let inventory = Inventory { shirts: vec![
+            ShirtColor::Red, ShirtColor::Red, ShirtColor::Blue
+        ]};
+
+        let color = inventory.most_stocked();
+
+        assert_eq!(ShirtColor::Red, color);
+    }
+
+    #[test]
+    fn ユーザが指定した色を返すこと() {
+        let inventory = Inventory { shirts: vec![
+            ShirtColor::Red, ShirtColor::Red, ShirtColor::Blue
+        ]};
+
+        let color = inventory.giveaway(Some(ShirtColor::Blue));
+
+        assert_eq!(ShirtColor::Blue, color);
+    }
+}

78e7d774d12f732208328db9930389c51c436c73
Kakeru Fukuda <stand.by.we@gmail.com>
2026-03-24 18:52:05 +0900

    searchにinsensitive版を追加

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -31,15 +31,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
 
-    result
+    contents.lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -47,13 +49,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
         
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+    
 }

936b92dda917e177290387057dce3e0e79caf0c5
Kakeru Fukuda <stand.by.we@gmail.com>
2026-03-24 14:02:10 +0900

    ファイルを読み込んで表示する

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,3 +1,40 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // コマンドラインから引数を受け取るためにはstd::env::args関数が必要
+    let args: Vec<String> = env::args().collect();
+    
+    // let query = &args[1];
+    let config = Config::new(&args);
+
+    println!("クエリ: {}", config.query);
+    println!("ファイル: {}", config.file);
+
+
+
+    let contents = fs::read_to_string(&config.file)
+        .expect("ファイルを読み込むことができるはずでしたが、失敗しました。");
+
+    println!("ファイルの中身:\n{contents}");
+
+
+}
+
+struct Config {
+    query: String,
+    file: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("引数が足りません")
+        }
+
+        let query = args[1].clone();
+        let file = args[2].clone();
+        
+        Config { query, file }
+    }
 }

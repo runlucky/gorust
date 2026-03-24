673d87d3e46620dc7137d9e358db5136f18cce74
Kakeru Fukuda <stand.by.we@gmail.com>
2026-03-25 08:45:56 +0900

    searchも分けちゃお

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -4,13 +4,15 @@ use std::fs;
 mod config;
 pub use config::Config;
 
+mod search;
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file)?;
 
     let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
+        search::search_case_insensitive(&config.query, &contents)
     } else {
-        search(&config.query, &contents)
+        search::search(&config.query, &contents)
     };
 
     for line in results {
@@ -18,52 +20,4 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    contents.lines()
-        .filter(|line| line.contains(query))
-        .collect()
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-
-    contents.lines()
-        .filter(|line| line.to_lowercase().contains(&query))
-        .collect()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn case_sensitive() {
-        let query = "duct";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Duct tape.";
-        
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-    }
-
-
-    #[test]
-    fn case_insensitive() {
-        let query = "rUsT";
-        let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
-        
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
-    }
-    
 }

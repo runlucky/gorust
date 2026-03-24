13f2c4feea8765b6b3eb279440c0fe3c93bbaadc
Kakeru Fukuda <stand.by.we@gmail.com>
2026-03-24 19:19:56 +0900

    エラーメッセージをエラー出力にする

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -6,14 +6,14 @@ use minigrep::Config;
 fn main() {
     // コマンドラインから引数を受け取るためにはstd::env::args関数が必要
     let args: Vec<String> = env::args().collect();
-    
+
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("引数解析時にエラーが発生しました: {err}");
+        eprintln!("引数解析時にエラーが発生しました: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("アプリケーションエラー: {e}");
+        eprintln!("アプリケーションエラー: {e}");
         process::exit(1);
     }
 }
